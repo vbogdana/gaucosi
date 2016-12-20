55a47a7abc1b9adf99ff3c46a4fc077502130e39
Dusan Spasojevic <dusanspasojevic7@gmail.com>
2016-12-20 16:23:46 +0100

    ubacena analitika

--- resources/views/home.php
+++ resources/views/home.php
@@ -6,6 +6,19 @@
         <meta charset="UTF-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title> Gaučosi - Belgrade Night Club</title>
+		
+						<script>
+				  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+				  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+				  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+				  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+				  ga('create', 'UA-89292527-1', 'auto');
+				  ga('send', 'pageview');
+
+						</script>
+				
+				
 		<meta property="fb:pages" content="gaucosi.belgrade">
 		<meta name="description" content="Gaucosi jedinstven nocni klub u Beogradu,koncept dopadljiv svima.Nalazi se u samom srcu Dorcola kod Kalemegdana.064 900 20 80">
 		<meta property="og:locale" content="en_US">
